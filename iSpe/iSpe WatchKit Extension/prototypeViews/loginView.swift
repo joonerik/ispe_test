d2d5584ea7062aa52200f7c2152cdc44ce7b4566
Joon-Erik Sæther <joon-erik@hotmail.com>
2022-02-15 11:10:07 +0100

    add .gitignore with template for swift, macos

---
+++ iSpe/iSpe WatchKit Extension/prototypeViews/loginView.swift
@@ -0,0 +1,20 @@
+//
+//  testviewSwiftUI.swift
+//  iSpe WatchKit Extension
+//
+//  Created by Joon-Erik Sæther on 15/02/2022.
+//
+
+import SwiftUI
+
+struct testviewSwiftUI: View {
+    var body: some View {
+        Text("Hello, Test!")
+    }
+}
+
+struct testviewSwiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        testviewSwiftUI()
+    }
+}
