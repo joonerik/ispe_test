99e1377140dce280dc171aa80fc8502f1519168f
Joon-Erik Sæther <joon-erik@hotmail.com>
2022-02-15 10:52:27 +0100

    create iSpe watch app project

---
+++ iSpe/iSpe WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iSpe WatchKit Extension
+//
+//  Created by Joon-Erik Sæther on 15/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iSpe/iSpe WatchKit Extension/iSpeApp.swift
@@ -0,0 +1,19 @@
+//
+//  iSpeApp.swift
+//  iSpe WatchKit Extension
+//
+//  Created by Joon-Erik Sæther on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct iSpeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
